8c144296c65bc4942592b04a08a3f8724d99a8f5
Macey Bell <maceytbell@gmail.com>
2022-11-02 14:23:07 -0600

    fixed html pathway

--- server/controllers/pageCtrl.js
+++ server/controllers/pageCtrl.js
@@ -1,6 +1,8 @@
 const path = require("path")
 
 module.exports = {
-   home: (req, res) => { res.sendFile(path.join(__dirname,"../index.html"))
+
+    home: (req, res) => {
+        res.sendFile(path.join(__dirname,"../../index.html"))
     }
 }
