703c5229a7052840cfed8b86256715f0dc60b14f
Macey Bell <maceytbell@gmail.com>
2022-11-03 15:23:34 -0600

    oh boy oh boy

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: process.env.ROLLBAR_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
 let basket = document.querySelector('.basket')
 
 function mouseImg (evt){
