9b3411bc403c770a3019fd5b1c18d37960b9b0e4
Macey Bell <maceytbell@gmail.com>
2022-11-03 15:18:42 -0600

    rollbar

--- server/index.js
+++ server/index.js
@@ -6,6 +6,16 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: process.env.ROLLBAR_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
 const {home, css, main} = require("./controllers/pageCtrl")
 
 
@@ -13,7 +23,11 @@ app.get("/", home)
 app.get("/css", css)
 app.get("/main", main)
 
-
+try {
+    ojiieje()
+} catch {
+    rollbar.critical('caught a backend failure')
+}
 
 
 const { PORT } = process.env
