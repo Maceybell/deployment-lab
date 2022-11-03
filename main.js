4d42cd66543e3f45feb3d281fb202caaae5a2087
Macey Bell <maceytbell@gmail.com>
2022-11-03 15:21:47 -0600

    rollbar info

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ let basket = document.querySelector('.basket')
 function mouseImg (evt){
 
     evt.target.classList.add("opacity")
+
+    rollbar.info('scrolled over picture')
 }
 
 function mouseOff (evt){
